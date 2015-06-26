801247df2e11eeffb7b01a95aecad6a3d50355bb
trevormunoz <trevor.munoz@gmail.com>
2015-06-26 09:35:54 -0400

    personal git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -17,3 +17,8 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+
+# More aliases
+alias gw='git wtf'
+alias ga='git add -p'
+alias gcm='git commit -m'
