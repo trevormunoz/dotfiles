d486b656452781d5f18e69ab7512cf9c6404d06b
trevormunoz <trevor.munoz@gmail.com>
2015-06-26 10:02:44 -0400

    Change git status alias

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -16,7 +16,7 @@ alias gca='git commit -a'
 alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gs='git status'
 
 # More aliases
 alias gw='git wtf'
