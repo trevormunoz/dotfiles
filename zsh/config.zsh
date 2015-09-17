7f80135d606adf2f9f549bb915903962406ee583
trevormunoz <trevor.munoz@gmail.com>
2015-09-17 17:34:10 -0400

    Add tmuxinator, zsh-syntax-highlighting, zsh-history-substring-search

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -48,3 +48,13 @@ bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+# bind UP and DOWN arrow keys
+zmodload zsh/terminfo
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+
+# bind UP and DOWN arrow keys (compatibility fallback
+# for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
